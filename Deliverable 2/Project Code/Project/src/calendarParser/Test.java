ff97a2edfbaac6d2cbf6baa577b1d15809c9db74
cherkaski <alexcher81@gmail.com>
2018-02-21 16:19:01 -0500

    Added acorn calendar file description to Test class.

--- Deliverable 2/Project Code/Project/src/calendarParser/Test.java
+++ Deliverable 2/Project Code/Project/src/calendarParser/Test.java
@@ -3,6 +3,12 @@ package calendarParser;
 import java.util.ArrayList;
 
 public class Test {
+	
+	// The acorn calendar breaks up all the classes into individual blocks.
+	// For example csc369 does not have one block, but three, like in your schedule.
+	// The parser returns a list of CalendarBlock object where each object represents a calendar block.
+	// The CalendarBlock objects themselves contain all the info about the block
+	// e.g. start time, end time, name, description, etc...
 
 	public static void main(String[] args) {
 		// For testing the parser.
