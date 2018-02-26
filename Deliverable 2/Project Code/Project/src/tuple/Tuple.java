60f3ad60b2ee6b1ca42757bdcecbe507bdd81aff
MrDeeky <dicky.zeng97@gmail.com>
2018-02-25 22:20:47 -0500

    Implemented helper function to determine day of the week given YYYYMMDD date format.

--- Deliverable 2/Project Code/Project/src/tuple/Tuple.java
+++ Deliverable 2/Project Code/Project/src/tuple/Tuple.java
@@ -3,15 +3,17 @@ package tuple;
 public class Tuple<T> {
 	private T item1 = null;
 	private T item2 = null;
+	private T item3 = null;
 	
-	public Tuple(T item1, T item2) {
-		if (item1 == null || item2 == null) {
+	public Tuple(T item1, T item2, T item3) {
+		if (item1 == null || item2 == null || item3 == null) {
 			throw new IllegalArgumentException();
 		}
 		this.item1 = item1;
 		this.item2 = item2;
+		this.item3 = item3;
 	}
-	
+
 	public T getItem1() {
 		return this.item1;
 	}
@@ -20,8 +22,12 @@ public class Tuple<T> {
 		return this.item2;
 	}
 	
+	public T getItem3() {
+		return this.item3;
+	}
+	
 	public boolean equal(Tuple<T> other) {
-		if (this.item1.equals(other.getItem1()) && this.item2.equals(other.getItem2())) {
+		if (this.item1.equals(other.getItem1()) && this.item2.equals(other.getItem2()) && this.item3.equals(other.getItem3())) {
 			return true;
 		}
 		return false;
@@ -32,6 +38,6 @@ public class Tuple<T> {
 	}
 	
 	public String toString() {
-		return "(" + this.item1 + ", " + this.item2 + ")";
+		return "(" + this.item1 + ", " + this.item2 + ", " + this.item3 + ")";
 	}
 }
