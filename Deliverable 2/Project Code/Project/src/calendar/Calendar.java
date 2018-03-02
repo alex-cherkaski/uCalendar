3d75fa963f97ef37ece6b65e291019e59c70dc87
Jordan <jordan.tram@mail.utoronto.ca>
2018-03-02 13:33:02 -0500

    Comments to Calendar class
    More comments

--- Deliverable 2/Project Code/Project/src/calendar/Calendar.java
+++ Deliverable 2/Project Code/Project/src/calendar/Calendar.java
@@ -23,7 +23,6 @@ public class Calendar {
 	// TODO: Method to identify conflicts and free slots with another Calendar object.
 	
 	// TODO: Method to find free time slots inside this Calendar object.
-	// TODO: Method to find mutually free time slots with another Calendar.
 	
 	public void removeEverything() {
 		this.courses.removeAllCourses();
