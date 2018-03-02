b17eca2a88796d3cfc31b6d4bb500e106e8e3655
Prince Minhas <prince4161@hotmail.com>
2018-03-02 13:02:11 -0500

    Added add course function

--- Deliverable 2/Project Code/Project/src/courseCollection/CourseCollection.java
+++ Deliverable 2/Project Code/Project/src/courseCollection/CourseCollection.java
@@ -1,5 +1,6 @@
 package courseCollection;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import course.Course;
@@ -7,10 +8,23 @@ import course.Course;
 public class CourseCollection {
 	private List<Course> courseList;
 	
+	public CourseCollection(){
+		this.courseList = new ArrayList<Course>();
+	}
+	
 	public CourseCollection(List<Course> courseList) {
 		if (courseList == null) {
 			throw new NullPointerException();
 		}
 		this.courseList = courseList;
+	}
+	
+	public void addCourse(Course course) {
+		if (course == null) {
+			throw new NullPointerException();
+		}
+		if (!this.courseList.contains(course)) {
+			this.courseList.add(course);
+		}
 	}
 }
