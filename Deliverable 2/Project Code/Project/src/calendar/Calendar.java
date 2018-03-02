4255064d534399a8a4911da2d78a00db51e3095d
Jordan <jordan.tram@mail.utoronto.ca>
2018-03-02 13:25:12 -0500

    Changed Calendar class
    Modified Calendar class to work with new EventsCollection and CourseCollection classes; done to have more modularity

--- Deliverable 2/Project Code/Project/src/calendar/Calendar.java
+++ Deliverable 2/Project Code/Project/src/calendar/Calendar.java
@@ -4,78 +4,34 @@ import java.util.List;
 
 import course.Course;
 import event.Event;
+import courseCollection.CourseCollection;
+import eventsCollection.EventsCollection;
 
 public class Calendar {
-	private List<Course> courseList;
-	private List<Event> eventList;
+	private CourseCollection courses;
+	private EventsCollection events;
 	
 	public Calendar(List<Course> courseList, List<Event> eventList) {
 		if (courseList == null || eventList == null) {
 			throw new NullPointerException();
 		}
-		this.courseList = courseList;
-		this.eventList = eventList;
+		this.courses = new CourseCollection(courseList);
+		this.events = new EventsCollection(eventList);
 	}
 	
 	// TODO: Method to identify conflicts inside this Calendar object.
 	// TODO: Method to identify conflicts and free slots with another Calendar object.
 	
-	public void addCourse(Course course) {
-		if (course == null) {
-			throw new NullPointerException();
-		}
-		if (!this.courseList.contains(course)) {
-			this.courseList.add(course);
-		}
-	}
-	
-	public void removeCourse(Course course) {
-		if (course == null) {
-			throw new NullPointerException();
-		}
-		if (!this.courseList.contains(course)) {
-			throw new IllegalArgumentException();
-		}
-		this.courseList.remove(course);
-	}
-	
-	public void addEvent(Event event) {
-		if (event == null) {
-			throw new NullPointerException();
-		}
-		if (!this.eventList.contains(event)) {
-			this.eventList.add(event);
-		}
-	}
-	
-	public void removeEvent(Event event) {
-		if (event == null) {
-			throw new NullPointerException();
-		}
-		if (!this.eventList.contains(event)) {
-			throw new IllegalArgumentException();
-		}
-		this.eventList.remove(event);
-	}
-	
-	public void removeAllCourses() {
-		this.courseList.clear();
-	}
-	
-	public void removeAllEvents() {
-		this.eventList.clear();
-	}
-	
 	public void removeEverything() {
-		this.courseList.clear();
-		this.eventList.clear();
+		this.courses.removeAllCourses();
+		this.events.removeAllEvents();
 	}
 	
-	public List<Course> getCourseList() {
-		return this.courseList;
+	public CourseCollection getCourseList() {
+		return this.courses;
 	}
 	
-	public List<Event> getEventList() {
-		return this.eventList;
+	public EventsCollection getEventList() {
+		return this.events;
 	}
 }
