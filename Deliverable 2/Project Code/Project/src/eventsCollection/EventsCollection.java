01998a77df5b2eea274ded21d9a9044c96781b0f
Jordan <jordan.tram@mail.utoronto.ca>
2018-03-02 13:03:14 -0500

    Modularizing code
    Making events list from Calendar into its own class for modularity

---
+++ Deliverable 2/Project Code/Project/src/eventsCollection/EventsCollection.java
@@ -0,0 +1,14 @@
+package eventsCollection;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import event.Event;
+
+public class EventsCollection {
+	private List<Event> eventList;
+	
+	public EventsCollection() {
+		this.eventList = new ArrayList<Event>();
+	}
+}
