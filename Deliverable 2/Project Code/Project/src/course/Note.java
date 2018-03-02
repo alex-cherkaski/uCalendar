6223665cb45fca7cd4ab6eed6477c7a3b1de9ed0
cherkaski <alexcher81@gmail.com>
2018-03-02 18:39:25 -0500

    Added system date finger print to Note class.

--- Deliverable 2/Project Code/Project/src/course/Note.java
+++ Deliverable 2/Project Code/Project/src/course/Note.java
@@ -1,16 +1,31 @@
 package course;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Note {
 	// Note ID used to distinguish notes.
 	private static int noteID = 0;
 	private int thisNoteID;
 	private String note;
-	private String date;
+	// Date used to fingerprint when the note was created. 
+	private DateFormat dateFormat;
+	private Date date;
 	
 	public Note(String note) {
 		noteID += 1;
 		this.thisNoteID = noteID;
 		this.note = note;
+		this.dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		this.date = new Date();
+	}
+	
+	public Note() {
+		noteID += 1;
+		this.thisNoteID = noteID;
+		this.dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		this.date = new Date();
 	}
 
 	public String getNote() {
@@ -25,16 +40,17 @@ public class Note {
 		return noteID;
 	}
 
+	public DateFormat getDateFormat() {
+		return dateFormat;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+	
 	@Override
 	public String toString() {
 		return "Note #" + this.thisNoteID;
 	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
+	
 }
