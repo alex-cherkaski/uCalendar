5f2e375ec75d2532840d361081e381e6ed251cfd
cherkaski <alexcher81@gmail.com>
2018-03-14 14:19:16 -0400

    Update Dropbox clinet class.

--- Project Code/Project/src/dropbox/DropboxClient.java
+++ Project Code/Project/src/dropbox/DropboxClient.java
@@ -1,5 +1,28 @@
 package dropbox;
 
-public class DropboxClient {
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.users.FullAccount;
 
+public class DropboxClient {
+	private static final String ACCESS_TOKEN = "";
+	private DbxRequestConfig config = null;
+	DbxClientV2 client = null;
+	FullAccount account = null;
+
+	public DropboxClient() {
+		// Create Dropbox client
+		config = new DbxRequestConfig("dropbox/java-tutorial");
+		client = new DbxClientV2(config, ACCESS_TOKEN);
+
+		try {
+			FullAccount account = client.users().getCurrentAccount();
+			System.out.println(account.getName().getDisplayName());
+		}
+		catch (DbxException dbxe)
+		{
+			dbxe.printStackTrace();
+		}
+	}
 }
