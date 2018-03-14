2682c833b9db3e252241675eda0bad36ae71aba2
cherkaski <alexcher81@gmail.com>
2018-03-14 14:10:23 -0400

    Got file uploading working.

--- Project Code/Project/src/dropbox/Test.java
+++ Project Code/Project/src/dropbox/Test.java
@@ -1,12 +1,25 @@
 package dropbox;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.dropbox.core.DbxApiException;
+import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.users.FullAccount;
 
 /*
  * IF YOU ARE GETTING ERRORS IN ECLIPSE HERE:
  * Go here: https://github.com/dropbox/dropbox-sdk-java/releases/tag/v3.0.6
  * Download all the .jar files to a folder on your desktop.
+ * Go here: https://stackoverflow.com/questions/44319563/noclassdeffounderror-while-using-dropbox-core-api
+ * Download jackson-core-x.x.x linked in the comments.
+ * THAT JACKSON BULLSHIT WAS NOT EVEN IN THE DOCUMENTATION YET
+ * YOU CANNOT RUN THE API WITHOUT! WTF!?
+ * WHAT KIND OF An ASSHOLE WROTE THE DOCUMENTATION FOR THIS!?
  * In Eclipse, right click on JRE System Library on the left.
  * Go to Build Path and click on Configure Build Path...
  * Click on Get External JARS...
@@ -15,11 +28,21 @@ import com.dropbox.core.v2.DbxClientV2;
  */
 
 public class Test {
-	private static final String ACCESS_TOKEN = "<ACCESS TOKEN>";
-	
-	public static void main(String[] args) {
-        // Create Dropbox client
-        DbxRequestConfig config = DbxRequestConfig.newBuilder("uCalendar/1.0").build();
-        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+	private static final String ACCESS_TOKEN = "";
+
+	public static void main(String[] args) throws DbxApiException, DbxException, IOException {
+		// Create Dropbox client
+		DbxRequestConfig config = DbxRequestConfig.newBuilder("uCalendar/1.0").build();
+		DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+
+		// Get current account info
+		FullAccount account = client.users().getCurrentAccount();
+		System.out.println(account.getName().getDisplayName());
+		
+		// Upload "test.txt" to Dropbox
+		try (InputStream in = new FileInputStream("C:\\Users\\user\\Desktop\\test.txt")) {
+		    FileMetadata metadata = client.files().uploadBuilder("/test.txt")
+		        .uploadAndFinish(in);
+		}
 	}
 }
