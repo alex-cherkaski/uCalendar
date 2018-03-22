fa5caddbb51232bcfcae42b25236d472c15305c1
cherkaski <alexcher81@gmail.com>
2018-03-22 10:27:59 -0400

    Added DropboxUtilities class. Updated Test class.

--- Project Code/Project/src/dropbox/TestClient.java
+++ Project Code/Project/src/dropbox/TestClient.java
@@ -7,10 +7,8 @@ import com.dropbox.core.DbxException;
 import com.dropbox.core.v2.files.UploadErrorException;
 
 public class TestClient {
-
 	public static void main(String[] args) throws UploadErrorException, FileNotFoundException, IOException, DbxException {
 		DropboxClient dbc = new DropboxClient();
-
 		// Alternate which tests you run to not get confused.
 //		dbc.uploadFile("C:\\Users\\user\\Desktop\\ClientClassTest.txt", "/ClientClassTest.txt");
 //		dbc.uploadFile("C:\\Users\\user\\Desktop\\test.txt", "/test.txt");
@@ -20,11 +18,13 @@ public class TestClient {
 		// Root folder on dropbox
 		for (String path : dbc.listFolder("")) {
 			System.out.println(path);
+			System.out.println("NAME: " + DropboxUtilities.getNameFromPath(path));
 		}
 		System.out.println();
 		// TestFolder located at root folder.
 		for (String path : dbc.listFolder("/TestFolder")) {
 			System.out.println(path);
+			System.out.println("NAME: " + DropboxUtilities.getNameFromPath(path));
 		}
 //		dbc.readFile("C:\\Users\\user\\Desktop", "/ClientClassTest.txt", "ClientClassTest.txt");
 	}
