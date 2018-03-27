c3eef14875339650b19dd75e4fd02de54ca69db3
Nghiem <nghiem.ly@mail.utoronto.ca>
2018-03-26 21:44:41 -0400

    removed unused imports in frontend classes

--- Project Code/Project/src/frontend_eventpage/EventWindowPanel.java
+++ Project Code/Project/src/frontend_eventpage/EventWindowPanel.java
@@ -1,6 +1,5 @@
 package frontend_eventpage;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -9,7 +8,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
